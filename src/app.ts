8712f0f0e8ee1c82bb6df57516539b975bb76159
Sebas de la Torre <lucasebasdlt@gmail.com>
2025-07-24 17:57:26 -0300

    feat:actualizacion de cors

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,11 @@ import paymentRoutes from "./routes/payment.routes"
 
 const app = express()
 
-app.use(cors())
+app.use(cors( {
+    origin: "http://localhost:3000",
+}
+
+))
 app.use(express.json())
 app.use("/api", paymentRoutes)
 
