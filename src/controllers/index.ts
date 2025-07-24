89e65414e0c87a2c67efee01dced3775116c2470
Sebas de la Torre <lucasebasdlt@gmail.com>
2025-07-24 17:22:16 -0300

    feat:debug finalizado para el controlador y servicio

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,12 +1,27 @@
-import {Request, Response} from "express";
+import { Request, Response } from "express";
 import { createPayment } from "../services/mercadopago.service";
 
 export const handleCreatePayment = async (req: Request, res: Response) => {
-    const {token, PaymentMethodId, email, items} = req.body;
-    try {
-        const payment = await createPayment(PaymentMethodId, token, email, items)
-        res.status(200).json({payment});
-    } catch (error) {
-        res.status(500).json({message: "Error al procesar el pago", error})
-    }
-}
+  const {
+    token,
+    PaymentMethodId,
+    email,
+    items,
+    description,
+    installments,
+  } = req.body;
+
+  try {
+    const payment = await createPayment(
+      PaymentMethodId,
+      token,
+      email,
+      items,
+      description,
+      installments
+    );
+    res.status(200).json({ payment });
+  } catch (error) {
+    res.status(500).json({ message: "Error al procesar el pago", error });
+  }
+};
